9196a03a906df6386f74cb16285da10dbd72bb71
RidusM <stormkillpeople@gmail.com>
2026-05-20 17:13:23 +0500

    fix: l2.14 fix goleaks

--- l2.14/main.go
+++ l2.14/main.go
@@ -2,71 +2,57 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-func sig(after time.Duration) <-chan interface{} {
-	c := make(chan interface{})
-	go func() {
-		defer close(c)
-		time.Sleep(after)
-	}()
-	return c
+func sig(after time.Duration) <-chan any {
+    c := make(chan any)
+    go func() {
+        defer close(c)
+        time.Sleep(after)
+    }()
+    return c
 }
 
 func main() {
-	start := time.Now()
-	<-OrFanIn(
-		sig(2*time.Hour),
-		sig(5*time.Minute),
-		sig(1*time.Second),
-		sig(1*time.Hour),
-		sig(1*time.Minute),
-	)
-	fmt.Printf("Done after: %v (expected ~1s)\n\n", time.Since(start))
+    start := time.Now()
+    <-Or(
+        sig(2*time.Hour),
+        sig(5*time.Minute),
+        sig(1*time.Second),
+        sig(1*time.Hour),
+        sig(1*time.Minute),
+    )
+    fmt.Printf("Done after: %v (expected ~1s)\n\n", time.Since(start))
 }
 
-func OrFanIn(channels ...<-chan interface{}) <-chan interface{} {
-	switch len(channels) {
-	case 0:
-		ch := make(chan interface{})
-		close(ch)
-		return ch
-	case 1:
-		return channels[0]
-	}
+func Or(channels ...<-chan any) <-chan any {
+    switch len(channels) {
+    case 0:
+        return nil
+    case 1:
+        return channels[0]
+    }
 
-	orDone := make(chan interface{})
+    orDone := make(chan any)
 
-	go func() {
-		defer close(orDone)
+    go func() {
+        defer close(orDone)
 
-		var wg sync.WaitGroup
-		wg.Add(len(channels))
+        switch len(channels) {
+        case 2:
+            select {
+            case <-channels[0]:
+            case <-channels[1]:
+            }
+        default:
+            mid := len(channels) / 2
+            select {
+            case <-Or(channels[:mid]...):
+            case <-Or(channels[mid:]...):
+            }
+        }
+    }()
 
-		signal := make(chan struct{}, 1)
-
-		for _, ch := range channels {
-			go func(c <-chan interface{}) {
-				defer wg.Done()
-				select {
-					case <-c:
-						select {
-							case signal <- struct{}{}:
-							default:
-						}
-					case <-signal:
-				}
-			}(ch)
-		}
-
-		<-signal
-
-		close(signal)
-
-		wg.Wait()
-	}()
-
-	return orDone
+    return orDone
 }
